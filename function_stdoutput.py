18d4dfafc2acc17423db3d35043f3317d1ec1013
Acemark M. Casumpang <acemark29casumpang@yahoo.com>
2016-02-26 16:23:30 +0800

    changed function name from Myfunc to Funcout

---
+++ function_stdoutput.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+nagan = "Mary"
+age = 19
+
+print" HI! my name is ", nagan + " and my age is ",age
+print" HI! my name is ", nagan + " and my age is ",age
+print" HI! my name is ", nagan + " and my age is ",age
+
